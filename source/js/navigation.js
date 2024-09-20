0a7db642d1f4a93e4dbe6e15b4ef5b10ebb08d26
Татьяна <tanya.karpova.ry@gmail.com>
2024-09-20 20:16:16 +0300

    Добавляет оверлей меню

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('.header__nav');
 const button = document.querySelector('.header__toggle');
+const body = document.querySelector('.page-body');
 
 function menuInteractive () {
   if (menu.className === 'header__nav header__nav--open') {
@@ -7,12 +8,15 @@ function menuInteractive () {
     button.classList.remove('header__toggle--open');
     menu.classList.add('header__nav--close');
     button.classList.add('header__toggle--close');
+    body.classList.remove('page-body--overlay');
   } else {
     menu.classList.remove('header__nav--close');
     button.classList.remove('header__toggle--close');
     menu.classList.add('header__nav--open');
     button.classList.add('header__toggle--open');
+    body.classList.add('page-body--overlay');
   }
 }
 
 menu.addEventListener('click', menuInteractive);
+
