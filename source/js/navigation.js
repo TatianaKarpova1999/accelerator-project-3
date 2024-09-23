d7934e7cc6695a64012ab28cad2ffcfe595aeed0
Татьяна <tanya.karpova.ry@gmail.com>
2024-09-23 12:24:49 +0300

    Дорабатывает интерактив меню

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,22 +1,22 @@
 const menu = document.querySelector('.header__nav');
 const button = document.querySelector('.header__toggle');
+const list = document.querySelector('.header__list');
 const body = document.querySelector('.page-body');
 
-function menuInteractive () {
-  if (menu.className === 'header__nav header__nav--open') {
-    menu.classList.remove('header__nav--open');
-    button.classList.remove('header__toggle--open');
-    menu.classList.add('header__nav--close');
-    button.classList.add('header__toggle--close');
-    body.classList.remove('page-body--overlay');
-  } else {
+
+body.addEventListener('click', (evt) => {
+  if (evt.target.className === 'header__nav header__nav--close' || evt.target.className === 'header__toggle header__toggle--close') {
     menu.classList.remove('header__nav--close');
     button.classList.remove('header__toggle--close');
     menu.classList.add('header__nav--open');
     button.classList.add('header__toggle--open');
     body.classList.add('page-body--overlay');
+  } else if (evt.target !== list && evt.target.offsetParent !== list){
+    menu.classList.remove('header__nav--open');
+    button.classList.remove('header__toggle--open');
+    menu.classList.add('header__nav--close');
+    button.classList.add('header__toggle--close');
+    body.classList.remove('page-body--overlay');
   }
-}
-
-menu.addEventListener('click', menuInteractive);
+});
 
