959f4c923fa9939f3b813e4b0dfa177d08e55b1e
Татьяна <tanya.karpova.ry@gmail.com>
2024-09-24 17:39:13 +0300

    Добавляет слайдер news

--- source/js/main.js
+++ source/js/main.js
@@ -3,3 +3,5 @@
 import '../js/navigation';
 import '../js/main-slider';
 import '../js/programs';
+import '../js/tabs';
+import '../js/news';
---
+++ source/js/tabs.js
@@ -0,0 +1,20 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+
+new Swiper('.swiper-tabs', {
+  loop: false,
+
+  breakpoints: {
+    320: {
+      allowTouchMove: true,
+      spaceBetween: 12,
+      slidesPerView: 2,
+      slidesPerGroup: 1,
+    },
+
+    768: {
+      allowTouchMove: false,
+      spaceBetween: 10,
+    },
+  }
+});
