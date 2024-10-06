d8a92df162e6bc397ee8a38d47882b4aafb895a0
Татьяна <tanya.karpova.ry@gmail.com>
2024-10-06 23:18:25 +0300

    Добавляет функции news

--- source/js/news.js
+++ source/js/news.js
@@ -4,7 +4,7 @@ import 'swiper/css';
 import 'swiper/css/grid';
 
 const pagination = document.querySelector('.swiper-pagination-news');
-const newsBlock = document.querySelector('.news')
+const newsBlock = document.querySelector('.news');
 const buttonPrev = newsBlock.querySelector('.swiper-button-prev');
 const buttonNext = newsBlock.querySelector('.swiper-button-next');
 
@@ -61,6 +61,41 @@ for (let i = 0; i <= 3; i++) {
   pagination.children[i].classList.add('swiper-pagination-bullet-main');
 }
 
+function checkingNextElement (i) {
+  if (pagination.children[i].className === 'swiper-pagination-bullet swiper-pagination-bullet-main swiper-pagination-bullet-active') {
+    if (pagination.children[i].nextSibling.className === 'swiper-pagination-bullet') {
+      pagination.children[i].nextSibling.classList.add('swiper-pagination-bullet-main');
+      pagination.children[pagination.children[i].innerHTML - 4].classList.remove('swiper-pagination-bullet-main');
+    }
+  }
+}
+
+function checkingPrevElement (i) {
+  if (pagination.children[i].className === 'swiper-pagination-bullet swiper-pagination-bullet-main swiper-pagination-bullet-active') {
+    if (pagination.children[i].previousElementSibling.className === 'swiper-pagination-bullet') {
+      pagination.children[i].previousElementSibling.classList.add('swiper-pagination-bullet-main');
+      pagination.children[parseInt(pagination.children[i].innerHTML, 10) + 2].classList.remove('swiper-pagination-bullet-main');
+    }
+  }
+}
+
+buttonNext.addEventListener('click', () => {
+  for (let i = 0; i <= pagination.children.length - 1; i++) {
+    if (pagination.children[i].nextSibling !== null) {
+      checkingNextElement(i);
+    }
+  }
+});
+
+buttonPrev.addEventListener('click', () => {
+  for (let i = 0; i <= pagination.children.length - 1; i++) {
+    if (pagination.children[i].previousElementSibling !== null) {
+      checkingPrevElement(i);
+    }
+  }
+});
+
+
 pagination.addEventListener('click', (evt) => {
   if (evt.target.nextSibling !== null && evt.target.previousElementSibling !== null) {
     if (evt.target.nextSibling.className === 'swiper-pagination-bullet') {
@@ -69,32 +104,6 @@ pagination.addEventListener('click', (evt) => {
     } else if (evt.target.previousElementSibling.className === 'swiper-pagination-bullet') {
       evt.target.previousElementSibling.classList.add('swiper-pagination-bullet-main');
       pagination.children[parseInt(evt.target.innerHTML, 10) + 2].classList.remove('swiper-pagination-bullet-main');
-    }
-  }
-});
-
-buttonNext.addEventListener('click', () => {
-  for (let i = 0; i <= pagination.children.length - 1; i++) {
-    if (pagination.children[i].nextSibling !== null) {
-      if (pagination.children[i].className === 'swiper-pagination-bullet swiper-pagination-bullet-main swiper-pagination-bullet-active') {
-        if (pagination.children[i].nextSibling.className === 'swiper-pagination-bullet') {
-          pagination.children[i].nextSibling.classList.add('swiper-pagination-bullet-main');
-          pagination.children[pagination.children[i].innerHTML - 4].classList.remove('swiper-pagination-bullet-main');
-        }
-      }
-    }
-  }
-});
-
-buttonPrev.addEventListener('click', () => {
-  for (let i = 0; i <= pagination.children.length - 1; i++) {
-    if (pagination.children[i].previousElementSibling !== null) {
-      if (pagination.children[i].className === 'swiper-pagination-bullet swiper-pagination-bullet-main swiper-pagination-bullet-active') {
-        if (pagination.children[i].previousElementSibling.className === 'swiper-pagination-bullet') {
-          pagination.children[i].previousElementSibling.classList.add('swiper-pagination-bullet-main');
-          pagination.children[parseInt(pagination.children[i].innerHTML, 10) + 2].classList.remove('swiper-pagination-bullet-main');
-        }
-      }
     }
   }
 });
