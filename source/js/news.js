70cc3d55086f03f95b53acfee6967bdda77a2de6
Татьяна <tanya.karpova.ry@gmail.com>
2024-10-06 17:35:56 +0300

    Проверяет переполнение

--- source/js/news.js
+++ source/js/news.js
@@ -3,6 +3,9 @@ import {Navigation, Pagination, Grid} from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/grid';
 
+const pagination = document.querySelector('.swiper-pagination-news');
+const bullet = pagination.querySelectorAll('span');
+
 new Swiper('.swiper-news', {
   modules: [Navigation, Pagination, Grid],
   loop: false,
@@ -12,7 +15,6 @@ new Swiper('.swiper-news', {
     clickable: true,
     type: 'bullets',
     dynamicBullets: true,
-    dynamicMainBullets: 3,
     renderBullet: function (index, className) {
       return `<span class="${className}">${index + 1}</span>`;
     },
@@ -55,3 +57,30 @@ new Swiper('.swiper-news', {
   },
 });
 
+let i = 0;
+let j = pagination.children.length;
+
+pagination.addEventListener('click', (evt) => {
+  // if (evt.target.innerHTML >= 4 && evt.target.nextElementSibling.className === 'swiper-pagination-bullet swiper-pagination-bullet-active-next') {
+  //   pagination.children[i].style.opacity = '0';
+  //   pagination.children[j - 1].style.opacity = '1';
+  //   i++;
+  //   console.log(evt.target.nextElementSibling.className);
+  // } else if (evt.target.innerHTML <= 5 && evt.target.previousElementSibling.className === 'swiper-pagination-bullet swiper-pagination-bullet-active-prev') {
+  //   pagination.children[j - 1].style.opacity = '0';
+  //   pagination.children[evt.target.innerHTML - 2].style.opacity = '1';
+  //   console.log(evt.target.innerHTML);
+  //   j--;
+  // }
+
+
+
+
+  console.log(evt.target.innerHTML);
+  for (i = 0; i < 8; i++)
+  {
+    console.log(pagination.children[i].className);
+  }
+
+
+});
